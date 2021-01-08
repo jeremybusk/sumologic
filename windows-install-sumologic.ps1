72f52edff051373e0e8412e5e108093c5a3126a3
Jeremy Busk <jeremybusk@gmail.com>
2021-01-08 15:53:07 -0700

    Update windows-install-sumologic.ps1

--- windows-install-sumologic.ps1
+++ windows-install-sumologic.ps1
@@ -1,5 +1,6 @@
 # Install and register sumologic collector via token with specified default sources.json
 # https://help.sumologic.com/03Send-Data/Installed-Collectors/05Reference-Information-for-Collector-Installation/04Add_a_Collector_to_a_Windows_Machine_Image
+# Usage: ./windows-install-sumologic -t <my secret token>
 
 $ErrorActionPreference = "Stop"
 Param(
