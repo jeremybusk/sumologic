30de3fc47c83c0ca09588f83ff04fa659a069b8f
Jeremy Busk <jeremybusk@gmail.com>
2021-01-11 09:57:05 -0700

    Update windows-install-sumologic.ps1

--- windows-install-sumologic.ps1
+++ windows-install-sumologic.ps1
@@ -10,21 +10,37 @@ Param(
   [string]$hostname = ($env:computerName).tolower()
 )
 $ErrorActionPreference = "Stop"
-
 $install_dir="C:\Sum"
-if(!(test-path $install_dir)){
-  New-Item -ItemType Directory -Force -Path $install_dir
+
+
+function install(){  
+  if(!(test-path $install_dir)){
+    New-Item -ItemType Directory -Force -Path $install_dir
+  }
+
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
+  Invoke-WebRequest 'https://collectors.us2.sumologic.com/rest/download/win64' -outfile 'C:\Windows\Temp\SumoCollector.exe'
+  Invoke-WebRequest 'https://raw.githubusercontent.com/jeremybusk/sumologic/master/windows_default_sources.json' -outfile "$install_dir\sources.json"
+  C:\Windows\Temp\SumoCollector.exe -console -q -Vclobber=True "-Vsumo.token_and_url=$token" "-Vcollector.name=$hostname_events" "-Vsources=$install_dir\"
 }
 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
-Invoke-WebRequest 'https://collectors.us2.sumologic.com/rest/download/win64' -outfile 'C:\Windows\Temp\SumoCollector.exe'
-Invoke-WebRequest 'https://raw.githubusercontent.com/jeremybusk/sumologic/master/windows_default_sources.json' -outfile "$install_dir\sources.json"
-C:\Windows\Temp\SumoCollector.exe -console -q -Vclobber=True "-Vsumo.token_and_url=$token" "-Vcollector.name=$hostname_events" "-Vsources=$install_dir\"
 
 # Tests
-function test_sumo_collector_service(){
+function test_sumo_collector_service_not_running(){
   if ((get-service -name sumo-collector).status -ne "Running"){
     write-host "ERROR: Serivce is not running. Install appears to have failed."
     exit 1
   }
 }
+
+
+function test_not_running(){
+  if ((get-service -name sumo-collector).status -eq "Running"){
+    write-host "ERROR: Serivce is already installed and running. Exiting install."
+    exit 1
+  }
+  
+  function main(){
+  test_not_running
+  install
+  }
