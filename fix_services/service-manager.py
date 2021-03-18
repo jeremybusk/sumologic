607087b63ae6678190dee69f2edae4356af5bae3
Jeremy Busk <jeremybusk@gmail.com>
2021-03-18 16:44:21 +0000

    add argparse and made a service

--- fix_services/service-manager.py
+++ fix_services/service-manager.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # FIPS? https://www.ansible.com/blog/new-libssh-connection-plugin-for-ansible-network
 # powershell commands will be encoded by default for many reasons. If you want logged turn on logging https://github.com/ansible/ansible/issues/50107#issuecomment-448442954
-import json
+import argparse
+from getpass import getpass
 import ipaddress
+import json
 import re
 import shlex
 import socket
@@ -10,29 +12,19 @@ import subprocess
 import sys
 import time
 from types import SimpleNamespace
-# from ssh_wrapper import SshClient
-# from winrm_wrapper import WinrmClient
-# from remotecmd_wrapper import WinrmClient
-# from remotecmd_wrapper import SshClient
-from remotecmd_wrapper import RcmdClient
 
 
-# from icmplib import ping
 from decouple import config
-# import nmap
-# from pythonping import ping
 import paramiko
-# from pylibsshext.errors import LibsshSessionException
-# from pylibsshext.session import Session
 import requests
 from requests.auth import HTTPBasicAuth
 import winrm
 
 
-def fix_stopped_collectors(url):
-    USERNAME = config('SUMO_USERNAME')
-    USERPASS = config('SUMO_USERPASS')
-    r = requests.get(url, auth=HTTPBasicAuth(USERNAME, USERPASS))
+from remotecmd_wrapper import RcmdClient
+
+def fix_stopped_collectors(url, sumo_access_id, sumo_access_key, username, userpass):
+    r = requests.get(url, auth=HTTPBasicAuth(sumo_access_id, sumo_access_key))
     if r.status_code != 200:
         print("ERROR: API 401: Invalid auth? Check user/pass or api token combo.")
         return 1
@@ -40,8 +32,6 @@ def fix_stopped_collectors(url):
     for item in items:
         host = item['name'].replace('_events', '')
         alive = item['alive']
-        # print(f"{host} alive = {alive}")
-
         ### Test Values ###
         # time.sleep(5)
         # host = "testhost"
@@ -62,7 +52,7 @@ def fix_stopped_collectors(url):
         if "windows" in os.lower():
             print(f"host: {host} alive: {alive} os: {os}")
             time.sleep(3)
-            restart_service_if_stopped(host, "sumo-collector")
+            restart_service_if_stopped(host, username, userpass, "sumo-collector")
         elif "linux" in os.lower():
             print(f"{host} operating system is Linux.")
         else:
@@ -81,43 +71,26 @@ def test_os_detect(host):
         return "unknown"
 
 
-def restart_service_if_stopped(host, servicename):
-    USERNAME = config('WINRM_USERNAME')
-    USERPASS = config('WINRM_USERPASS')
+def restart_service_if_stopped(host, username, userpass, servicename):
     print(f"{host}: Checking service status for {servicename}.")
     cmd = f"(get-service {servicename}).Status"
-    # r = rcmd(host, cmd)
-    # s = RcmdClient(host, USERNAME, USERPASS, transport='ntlm')
     try:
-        s = RcmdClient(host, USERNAME, USERPASS, transport='ntlm', server_cert_validation='ignore')
+        s = RcmdClient(host, username, userpass, transport='ntlm', server_cert_validation='ignore')
         rsp = s.execute(cmd)
         status = rsp['out'].strip()
         print(status)
     except:
         return
-    # s = RcmdClient(host, USERNAME, USERPASS, transport='ntlm')
-    # s = RcmdClient(host, USERNAME, USERPASS)
-    # print(r.stdout)
-    # print(rsp['out'])
-    # print('foo')
-    # sys.exit()
     if status == "Stopped":
         print('================================')
         print(f"{host} starting stopped service {servicename}")
         print('================================')
-        # time.sleep(5)
-        # host = "txd1-enrapply"
-        # cmd = "get-eventlog system -n 3 | Select-Object -Property * | findstr -i sumologic"
         cmd = f"start-service {servicename}"
         try:
-          s = RcmdClient(host, USERNAME, USERPASS, transport='ntlm', server_cert_validation='ignore')
+          s = RcmdClient(host, username, userpass, transport='ntlm', server_cert_validation='ignore')
         except:
             return
-        # s = RcmdClient(host, USERNAME, USERPASS, transport='ntlm')
         rsp = s.execute(cmd)
-        # print(rsp)
-        # print(r.stdout)
-        # print(r.stderr)
     elif rsp == "Running":
         pass
         # print(f"{host} service status: running")
@@ -155,39 +128,58 @@ def test_tcp_port_open(host, port):
         return 1
 
 
-def test_ssh_rcmd():
-    USERNAME = config('SSH_USERNAME')
-    USERPASS = config('SSH_USERPASS')
-    # s = RcmdClient('lxd0-sandbox', USERNAME, USERPASS)
-    s = RcmdClient('lxd0-sandbox', USERNAME, USERPASS, rshport=22)
-    rsp = s.execute("ip address")
+def test_ssh_rcmd(host, username, userpass,  cmd='ip address'):
+    s = RcmdClient(host, username, userpass, rshport=22)
+    rsp = s.execute(cmd)
     print(rsp['cmd'])
     print(rsp['out'])
     print(rsp['err'])
     print(rsp['exit_status'])
 
 
-def test_winrm_rcmd():
-    USERNAME = config('WINRM_USERNAME')
-    USERPASS = config('WINRM_USERPASS')
-    s = RcmdClient('ws-w10', USERNAME, USERPASS, transport='ssl', server_cert_validation='ignore')
-    rsp = s.execute("ipconfig /all")
+def test_winrm_rcmd(host, username, userpass, cmd='ipconfig /all'):
+    s = RcmdClient(host, username, userpass, transport='ssl', server_cert_validation='ignore')
+    rsp = s.execute(cmd)
     print(rsp['cmd'])
     print(rsp['out'])
     print(rsp['err'])
     print(rsp['exit_status'])
 
 
-def test_win_service_restart():
-    USERNAME = config('WINRM_USERNAME')
-    USERPASS = config('WINRM_USERPASS')
-    restart_service_if_stopped('ws-w10', 'svsvc')
+def test_win_service_restart(host, username, userpass, service='sumo-collector'):
+    restart_service_if_stopped(host, username, userpass, service)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run systems service checker.')
+    parser.add_argument('-s', '--sumo-access-id', required=False, type=str,
+                        help='Sumo access id.')
+    parser.add_argument('-u', '--username', required=False, type=str,
+                        help='Windows AD username')
+    parser.add_argument('-i', '--interval-in-seconds', required=True, type=int,
+                        help='Interval in seconds to check sumologic.com and try and restart service with "Stopped Collector" status.')
+    args = parser.parse_args()
+    if args.username:
+        args.password = getpass(prompt='Windows AD user password: ')
+    else:
+        args.username = config('USERNAME')
+        args.password = config('USERPASS')
+    if args.sumo_access_id:
+        args.sumo_access_key = getpass(prompt="Sumologic access id access key")
+    else:
+        args.sumo_access_id = config('SUMO_ACCESS_ID')
+        args.sumo_access_key = config('SUMO_ACCESS_KEY')
+
+    while True:
+        fix_stopped_collectors("https://api.us2.sumologic.com/api/v1/collectors", args.sumo_access_id, args.sumo_access_key, args.username, args.password)
+        time.sleep(args.interval_in_seconds)
 
 
 if __name__ == "__main__":
+    main()
     # USERNAME = config('REMOTESHELL_USERNAME')
     # USERPASS = config('REMOTESHELL_USERPASS')
-    # test_ssh_rcmd()
-    # test_winrm_rcmd()
+    # test_ssh_rcmd(myhost)
+    # test_winrm_rcmd(myhost)
     # test_win_service_restart()
-    fix_stopped_collectors("https://api.us2.sumologic.com/api/v1/collectors")
+    # fix_stopped_collectors("https://api.us2.sumologic.com/api/v1/collectors")
