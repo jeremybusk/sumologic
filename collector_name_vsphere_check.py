823f45494320cb5b7859149546e3c69cb25fa064
Jeremy Busk <jeremybusk@gmail.com>
2021-06-07 12:36:19 -0600

    add delete for powered off

--- collector_name_vsphere_check.py
+++ collector_name_vsphere_check.py
@@ -6,19 +6,54 @@ import urllib3
 from vmware.vapi.vsphere.client import create_vsphere_client
 from sumologic import SumoLogic
 
+
+# Vsphere
 vc_username = ""
 vc_userpass = ""
+vsphere1_host = ''
+vsphere2_host = ''
+# Sumo
 sumo_access_id = ""
 sumo_access_key = ""
 
-vsphere2_host = ''
-vsphere1_host = ''
+
+sumo = SumoLogic(sumo_access_id, sumo_access_key)
+
+
+def delete_collector(sumo_collector):
+    url = f"https://api.us2.sumologic.com/api/v1/collectors/{sumo_collector['id']}"
+    r = requests.delete(url, auth=(sumo_access_id, sumo_access_key))
+    print(f"I: Deleting {sumo_collector['id']} {sumo_collector['name']} rsp: {r}")
+    return r
+
+
+def delete_powered_off_vm_sumo_collectors(sumo_collectors, vms):
+    for sumo_collector in sumo_collectors:
+        if sumo_collector['alive'] == False:
+            match_ratio, match_vm = get_vm_name_in_sumo_collector_name_ratio(sumo_collector, vms)
+            print(f"Collector: {sumo_collector['name']} VM: {match_vm.name} POWER: {match_vm.power_state} MatchRatio: {match_ratio}")
+            if match_ratio > .9 and match_vm.power_state == "POWERED_OFF":
+                delete_collector(sumo_collector)
 
 
 def get_sumo_collectors():
-    sumo = SumoLogic(sumo_access_id, sumo_access_key)
     sumo_collectors = sumo.collectors()
     return sumo_collectors
+
+
+def get_vm_name_in_sumo_collector_name_ratio(sumo_collector, vms):
+    match_ratio = 0
+    for vm in vms:
+        vm_name = vm.name.lower()
+        sumo_name = sumo_collector['name'].lower().replace('_events', '')
+        if sumo_name in vm_name:
+            new_match_ratio = .95
+        else:
+            new_match_ratio = SequenceMatcher(a=vm_name,b=sumo_name).ratio()
+        if new_match_ratio > match_ratio:
+            match_vm = vm
+            match_ratio = new_match_ratio
+    return match_ratio, match_vm
 
 
 def get_vsphere_vms(vc_host):
@@ -30,27 +65,13 @@ def get_vsphere_vms(vc_host):
     return vms
 
 
-def get_vm_name_in_sumo_collector_name_ratio(sumo_collector, vms):
-    match_ratio = 0
-    for vm in vms:
-        vm_name = vm.name.lower()
-        sumo_name = sumo_collector['name'].lower().replace('_events', '')
-        new_match_ratio = SequenceMatcher(a=vm_name,b=sumo_name).ratio()
-        if new_match_ratio > match_ratio:
-            match_vm = vm
-            match_ratio = new_match_ratio
-    return match_ratio, match_vm
-
 
 def main():
     vsphere1_vms = get_vsphere_vms(vsphere1_host)
     vsphere2_vms = get_vsphere_vms(vsphere2_host)
     vms =  vsphere1_vms + vsphere2_vms
     sumo_collectors = get_sumo_collectors()
-    for sumo_collector in sumo_collectors:
-        if sumo_collector['alive'] == False:
-            match_ratio, match_vm = get_vm_name_in_sumo_collector_name_ratio(sumo_collector, vms)
-            print(f"Collector: {sumo_collector['name']} VM: {match_vm.name} POWER: {match_vm.power_state} MatchRatio: {match_ratio}")
+    delete_powered_off_vm_sumo_collectors(sumo_collectors, vms)
 
 
 if __name__ == "__main__":
---
+++ collector_name_vsphere_check_readme.md
@@ -0,0 +1,2 @@
+pip install --upgrade pip setuptools
+pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
