2c77d73f6158ac56eab157bdef7d34b1f2c2631b
Jeremy Busk <jeremybusk@gmail.com>
2021-07-09 00:52:58 -0600

    Update sumo-install.ps1

--- sumo-install.ps1
+++ sumo-install.ps1
@@ -1,5 +1,5 @@
 $cmd = "C:\Windows\Temp\SumoCollector.exe"
-$args = "-console -q -Vclobber=True -Vsumo.token_and_url=<your token> -Vcollector.name=wjp1-itsupport_events -Vsources=\Windows\Temp\sources.json"
+$args = "-console -q -Vclobber=True -Vsumo.token_and_url=<your token> -Vcollector.name=myhost_events -Vsources=\Windows\Temp\sources.json"
 $p = Start-Process $cmd $args -Wait -Passthru
 $p.WaitForExit()
 if ($p.ExitCode -ne 0) {
