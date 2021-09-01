dcfd7e769e90e0dca3ad8063bbcd1d68eb078ab7
Jeremy Busk <jeremy.busk@extendhealth.com>
2021-09-01 11:48:41 -0600

    Create debian_install_sumologic.sh

---
+++ debian_install_sumologic.sh
@@ -0,0 +1,13 @@
+set -e
+
+sdir=$(pwd)
+token=$SUMO_TOKEN
+clobber=False  # True/False
+install_dir="/tmp/sumo"
+hostname=$(hostname | tr '[:upper:]' '[:lower:]')
+mkdir -p $install_dir
+cd $install_dir
+wget "https://collectors.sumologic.com/rest/download/linux/64" -O SumoCollector.sh && chmod +x SumoCollector.sh
+curl -L "https://raw.githubusercontent.com/jeremybusk/sumologic/master/linux_ubuntu_default_sources.json" -o "$install_dir/sources.json"
+./SumoCollector.sh -console -q "-Vclobber=$clobber" "-Vsumo.token_and_url=$token" "-Vcollector.name=${hostname}_events" "-Vsources=$install_dir/"
+cd $sdir
